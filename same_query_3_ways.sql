42e78a6e2b8ad9647d10298b5e079b160774bfdb
Steph Gooch <awssteph@amazon.com>
2024-10-08 14:14:54 +0100

    updated 3 query to have all the same names

--- same_query_3_ways.sql
+++ same_query_3_ways.sql
@@ -4,25 +4,24 @@ select line_item_usage_account_id, product_product_name, round(sum(line_item_unb
 from cur
 where month = '8' and year ='2024'
 group by line_item_usage_account_id, product_product_name
-order by product_product_name DESC
+order by cost DESC
 
 --CUR 2.0
 select line_item_usage_account_name, product['product_name'] as product_name, round(sum(line_item_unblended_cost),2) as cost 
 from cur2
 where SPLIT(billing_period,'-')[2] = '08'
 group by product['product_name'], line_item_usage_account_name 
+order by cost DESC
 
 
 --FOCUS
 SELECT
   ProviderName,
-  BillingCurrency,
   ServiceName,
   round(SUM(BilledCost),2) AS TotalBilledCost
 FROM "cid_data_export"."focus"
 where SPLIT(billing_period,'-')[2] = '08'
 GROUP BY
   ProviderName,
- BillingCurrency,
   ServiceName
---ORDER BY TotalBilledCost DESC
+ORDER BY TotalBilledCost DESC
