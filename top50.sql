1904b4c7da947aa028ea43eb1985891c1b4745de
aws-jrmarks <72207418+aws-jrmarks@users.noreply.github.com>
2024-11-29 13:25:50 -0500

    Update top50.sql

--- top50.sql
+++ top50.sql
@@ -4,14 +4,14 @@
 SELECT 
   line_item_resource_id,
   line_item_usage_account_name,
-	product [ 'product_name' ] AS "product_product_name",
-	DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
-	sum(line_item_unblended_cost) AS cost
+  product [ 'product_name' ] AS "product_product_name",
+  DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
+  SUM(line_item_unblended_cost) AS cost
 FROM 
   "cid_data_export"."cur2"
 WHERE 
   line_item_resource_id <> '' -- we don't want usage that doesn't have a resourceid
-	AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '3' DAY -- 3 days ago from today
+  AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '3' DAY -- 3 days ago from today
 GROUP BY 1,2,3,4
 
 ------------------------------------------------------------------------------------------
@@ -22,14 +22,14 @@ GROUP BY 1,2,3,4
 SELECT 
   line_item_resource_id,
   line_item_usage_account_name,
-	product [ 'product_name' ] AS "product_product_name",
-	DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
-	sum(line_item_unblended_cost) AS cost
+  product [ 'product_name' ] AS "product_product_name",
+  DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
+  SUM(line_item_unblended_cost) AS cost
 FROM 
   "cid_data_export"."cur2"
 WHERE 
   line_item_resource_id <> ''
-	AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '2' DAY -- changed the interval here
+  AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '2' DAY -- changed the interval here
 GROUP BY 1,2,3,4
 
 ------------------------------------------------------------------------------------------
@@ -38,33 +38,33 @@ GROUP BY 1,2,3,4
 -- wrap these queries with WITH in prep to join them up
 
 WITH three_days_ago AS (
-	SELECT 
+  SELECT 
     line_item_resource_id,
-		line_item_usage_account_name,
-		product [ 'product_name' ] AS "product_product_name",
-		DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
-		sum(line_item_unblended_cost) AS cost
-	FROM 
+    line_item_usage_account_name,
+    product [ 'product_name' ] AS "product_product_name",
+    DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
+    sum(line_item_unblended_cost) AS cost
+  FROM 
     "cid_data_export"."cur2"
-	WHERE 
+  WHERE 
     line_item_resource_id <> ''
-		AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '3' DAY
-	GROUP BY 1,2,3,4
+    AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '3' DAY
+  GROUP BY 1,2,3,4
 ),
 
 two_days_ago AS (
-	SELECT 
+  SELECT 
     line_item_resource_id,
-		line_item_usage_account_name,
-		product [ 'product_name' ] AS "product_product_name",
-		DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
-		sum(line_item_unblended_cost) AS cost
-	FROM 
+    line_item_usage_account_name,
+    product [ 'product_name' ] AS "product_product_name",
+    DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
+    SUM(line_item_unblended_cost) AS cost
+  FROM 
     "cid_data_export"."cur2"
-	WHERE 
+  WHERE 
     line_item_resource_id <> ''
-		AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '2' DAY
-	GROUP BY 1,2,3,4
+    AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '2' DAY
+  GROUP BY 1,2,3,4
 )
 
 ------------------------------------------------------------------------------------------
@@ -73,48 +73,48 @@ two_days_ago AS (
 -- Writing the final query, where we join the two result sets, calculate the cost delta and percent delta
 
 WITH three_days_ago AS (
-	SELECT 
+  SELECT 
     line_item_resource_id,
-		line_item_usage_account_name,
-		product [ 'product_name' ] AS "product_product_name",
-		DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
-		sum(line_item_unblended_cost) AS cost
-	FROM 
+    line_item_usage_account_name,
+    product [ 'product_name' ] AS "product_product_name",
+    DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
+    SUM(line_item_unblended_cost) AS cost
+  FROM 
     "cid_data_export"."cur2"
-	WHERE 
+  WHERE 
     line_item_resource_id <> ''
-		AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '3' DAY
-	GROUP BY 1,2,3,4
+    AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '3' DAY
+  GROUP BY 1,2,3,4
 ),
 
 two_days_ago AS (
-	SELECT 
+  SELECT 
     line_item_resource_id,
-		line_item_usage_account_name,
-		product [ 'product_name' ] AS "product_product_name",
-		DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
-		sum(line_item_unblended_cost) AS cost
-	FROM 
+    line_item_usage_account_name,
+    product [ 'product_name' ] AS "product_product_name",
+    DATE_FORMAT(line_item_usage_start_date, '%Y-%m-%d') AS day_line_item_usage_start_date,
+    SUM(line_item_unblended_cost) AS cost
+  FROM 
     "cid_data_export"."cur2"
-	WHERE 
+  WHERE 
     line_item_resource_id <> ''
-		AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '2' DAY
-	GROUP BY 1,2,3,4
+    AND line_item_usage_start_date = CURRENT_DATE - INTERVAL '2' DAY
+  GROUP BY 1,2,3,4
 )
 
 SELECT 
   three_days_ago.*,
-	two_days_ago.*,
-	(two_days_ago.cost - three_days_ago.cost) AS cost_delta,
-	(
-		(two_days_ago.cost - three_days_ago.cost) / three_days_ago.cost * 100
-	) AS percent_delta
+  two_days_ago.*,
+  (two_days_ago.cost - three_days_ago.cost) AS cost_delta,
+  (
+    (two_days_ago.cost - three_days_ago.cost) / three_days_ago.cost * 100
+  ) AS percent_delta
 FROM 
   three_days_ago
-	FULL OUTER JOIN two_days_ago ON three_days_ago.line_item_resource_id = two_days_ago.line_item_resource_id
+  FULL OUTER JOIN two_days_ago ON three_days_ago.line_item_resource_id = two_days_ago.line_item_resource_id
 ORDER BY 
   cost_delta DESC,
-	percent_delta DESC
+  percent_delta DESC
 LIMIT 50;
 
 ------------------------------------------------------------------------------------------
